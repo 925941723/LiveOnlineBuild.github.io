2f372f77fd75ab4e7da2aa15e50c722335ebc9a8
ZengSheng <925941723@qq.com>
2021-12-31 14:42:24 +0800

    新版本

--- src/settings.bf9eb.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/MainPage.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"803f8",main:"f43b3"}};
